c7bd35f690ddfa8d4ec0fb02a951077e6b961246
LauraKuzara <kuzaralaura@gmail.com>
2024-01-27 11:29:51 -0500

    wrote parties func

--- partyPlanner.js
+++ partyPlanner.js
@@ -1,13 +1,12 @@
-/*
-Fetch is used correctly to GET party data from the API.
-Fetch is used correctly to POST a new party to the API.
-Fetch is used correctly to DELETE a party from the API.
-The app contains a list of the names, dates, times, locations, and descriptions of all parties.
-Each party in the list has a delete button which removes the party when clicked.
-The app contains a form that allows a user to enter information about a party and add it to the list.
-The DOM is dynamically rendered according to data stored in state.
-The data stored in state is updated to stay in sync with the API.
-*/
+// Fetch is used correctly to GET party data from the API.
+// Fetch is used correctly to POST a new party to the API.
+// Fetch is used correctly to DELETE a party from the API.
+// The app contains a list of the names, dates, times, locations, and descriptions of all parties.
+// Each party in the list has a delete button which removes the party when clicked.
+// The app contains a form that allows a user to enter information about a party and add it to the list.
+// The DOM is dynamically rendered according to data stored in state.
+// The data stored in state is updated to stay in sync with the API.
+
 
 //store API URL
 const cohort = "2311-FSA-ET-WEB-PT-SF";
@@ -31,7 +30,7 @@ render();
 
 async function getEvents() {
     try {
-const response = await fetch(API_URL);
+const response = await fetch(APIURL);
 const json = await response.json();
     console.log(json);
     partyPlans.partyTime = json.data;
@@ -59,4 +58,28 @@ function renderpartyTime() {
     });
     console.log(eventCards, partyPlans.partyTime);
     eventList.replaceChildren(...eventCards);
+}
+
+async function parties(event) {
+    event.preventDefault();
+    try {
+    const response = await fetch(APIURL, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+        name: addeventForm.name.value,
+        description: addEventForm.description.value,
+        date: new Date (addEventForm.date.value),
+        location: addEventForm.location.value,
+        }),
+    });
+
+    if (!response.ok) {
+        throw new Error("Failed to create party");
+}
+
+    render();
+    } catch (error) {
+    console.error(error);
+    }
 }
