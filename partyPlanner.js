b6277805be3906034186ed31d415936add7db0bf
LauraKuzara <kuzaralaura@gmail.com>
2024-01-27 10:52:11 -0500

    added API URL

---
+++ partyPlanner.js
@@ -0,0 +1,15 @@
+/*
+Fetch is used correctly to GET party data from the API.
+Fetch is used correctly to POST a new party to the API.
+Fetch is used correctly to DELETE a party from the API.
+The app contains a list of the names, dates, times, locations, and descriptions of all parties.
+Each party in the list has a delete button which removes the party when clicked.
+The app contains a form that allows a user to enter information about a party and add it to the list.
+The DOM is dynamically rendered according to data stored in state.
+The data stored in state is updated to stay in sync with the API.
+*/
+
+//store API URL
+const cohort = "2311-FSA-ET-WEB-PT-SF";
+const APIURL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/${cohort}/events`;
+
